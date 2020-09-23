5766d8bdc6fe660a7e3c11343267020c7b71441c
Meet-Kalariya <kalariyameetd@gmail.com>
2020-09-23 14:10:16 +0530

    List of list is done but adding null is left

--- lib/optimal.dart
+++ lib/optimal.dart
@@ -1,59 +1,59 @@
-bool search(int key, List<int> fr){
-  for(int i=0; i<fr.length; i++){
-    if(fr.elementAt(i) == key)
-      return true;
+
+List<List> toprint = new List();
+int hit = 0;
+int miss = 0;
+
+bool search(int key, List<int> fr) {
+  for (int i = 0; i < fr.length; i++) {
+    if (fr.elementAt(i) == key) return true;
   }
   return false;
 }
 
-int predict(List<int> pg, List<int> fr, int pn, int index){
+int predict(List<int> pg, List<int> fr, int pn, int index) {
   int res = -1, farthest = index;
-  for(int i=0; i<fr.length; i++){
+  for (int i = 0; i < fr.length; i++) {
     int j;
-    for(j=index ; j<pn; j++){
-      if(fr.elementAt(i) == pg.elementAt(j)){
-        if( j > farthest){
+    for (j = index; j < pn; j++) {
+      if (fr.elementAt(i) == pg.elementAt(j)) {
+        if (j > farthest) {
           farthest = j;
           res = i;
         }
         break;
       }
     }
-    if(j == pn)
-      return i;
+    if (j == pn) return i;
   }
   return (res == -1) ? 0 : res;
 }
 
-void optimalpage(List<int> pg, int pn, int fn){
+void optimalpage(List<int> pg, int pn, int fn) {
   List<int> fr = new List();
-
-  int hit=0;
-  int miss=0;
-  for(int i=0; i<pn; i++){
-    print(fr);
-    if(search(pg.elementAt(i),fr)){
-      hit ++;
+  for (int i = 0; i < pn; i++) {
+    List<int> s1 = new List();
+    if (search(pg.elementAt(i), fr)) {
+      s1.addAll(fr);
+      toprint.add(s1);
+      hit++;
       continue;
     }
-    if(fr.length < fn){
+    if (fr.length < fn) {
       fr.add(pg.elementAt(i));
-    }
-    else{
-      int j = predict(pg, fr, pn, i+1);
+      s1.addAll(fr);
+      toprint.add(s1);
+    } else {
+      int j = predict(pg, fr, pn, i + 1);
       fr.removeAt(j);
       fr.insert(j, pg.elementAt(i));
-
+      s1.addAll(fr);
+      toprint.add(s1);
     }
   }
   miss = pn - hit;
-  print("No of hits = $hit");
-  print("No of miss = $miss");
 }
-void main() {
-  List<int> pages = [7, 0, 1, 2, 0, 3, 0, 4,
-    2, 3, 0, 3, 2];
-
-  int capacity = 4;
-  optimalpage(pages, pages.length, capacity);
+void main(){
+  List<int> pgs = [1,4,2,6,8,7,3,2];
+  optimalpage(pgs, pgs.length, 3);
+  print(toprint);
 }
