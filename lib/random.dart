7fb18f94f90474160e3a95e5400268aaa5015509
Meet-Kalariya <kalariyameetd@gmail.com>
2020-09-23 14:18:28 +0530

    List of list is done,adding null to list not done

--- lib/random.dart
+++ lib/random.dart
@@ -1,40 +1,41 @@
 import 'dart:math';
-int randompage(List<int> pages,int n, int capacity){
-  List<int> frame = new List();
-  int fault=0;
-  int hit=0;
-  var j = new Random();
-  for(int i=0; i < n; i++){
-    int n = j.nextInt(capacity);
-    //print(n);
-    if(frame.length < capacity){
-      if(!frame.contains(pages[i])){
-        print(frame);
-        frame.add(pages[i]);
-        fault++;
-      }
-    }
-    else{
-      if(frame.contains(pages[i])){
-        print(frame);
-        hit++;
-      }
-      else{
-        frame.removeAt(n);
-        frame.insert(n, pages[i]);
-        print(frame);
-        fault++;
 
+List<List> toprint = new List();
+int randompage(List<int> pages, int n, int capacity) {
+  List<int> s = new List();
+  int pagefault = 0;
+  int hit = 0;
+  var j = new Random();
+  for (int i = 0; i < n; i++) {
+    List<int> s1 = new List();
+    int n = j.nextInt(capacity);
+    if (s.length < capacity) {
+      if (!s.contains(pages[i])) {
+        print(s);
+        s.add(pages[i]);
+        s1.addAll(s);
+        toprint.add(s1);
+        pagefault++;
+      }
+    } else {
+      if (s.contains(pages[i])) {
+        s1.addAll(s);
+        toprint.add(s1);
+        hit++;
+      } else {
+        s.removeAt(n);
+        s.insert(n, pages[i]);
+        s1.addAll(s);
+        toprint.add(s1);
+        pagefault++;
       }
     }
   }
   print(hit);
-  return fault;
+  return pagefault;
 }
-void main() {
-  List<int> pages = [7, 0, 1, 2, 0, 3, 0, 4,
-    2, 3, 0, 3, 2];
-
-  int capacity = 4;
-  print(randompage(pages,pages.length, capacity));
+void main(){
+  List<int> pgs = [1,4,2,6,8,7,3,2];
+  randompage(pgs, pgs.length, 3);
+  print(toprint);
 }
